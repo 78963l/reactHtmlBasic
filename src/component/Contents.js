658375ee8055364d17223425665c34721c205a08
78963l <78963l@naver.com>
2022-03-19 23:48:00 +0900

    contents를 하나로 만들어 component로 붙여봄.

---
+++ src/component/Contents.js
@@ -0,0 +1,24 @@
+const contents = function(){
+    return (
+        <>
+      <ol style={{
+        textAlign: "left",
+      }}>
+        <li>
+          <a href="./1.html">기술소개</a>
+        </li>
+        <li>
+          <a href="./2.html">기본문법</a>
+        </li>
+        <li>
+          <a href="./3.html">하이퍼텍스트와 속성</a>
+        </li>
+        <li>
+          <a href="./4.html">리스트와 태크의 중첩</a>
+        </li>
+      </ol>
+        </>
+    );
+}
+
+export default contents
